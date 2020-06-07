5d79f53c5f62a0a8a9b19e503d6c105c622e1a03
Adam Halim <adaab301@student.liu.se>
2020-06-07 20:05:32 +0000

    Added xlsx package
    Currently only takes an .xls file and converts it to .csv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const xlsx = require('xlsx');
 
 const app = express();
-
 const server = app.listen(3000, () => {
     const host  = '0.0.0.0';
     const port = 3000;
     console.log(`Roomie listening on http://${host}:${port}`);
 });
+
+const workBook = xlsx.readFile('data/data.xls');
+xlsx.writeFile(workBook, 'data/output.csv', {bookType: "csv"});
