5beb83b437a2d985bac199eacf7bbadfd322e2d6
Adam Halim <adaab301@student.liu.se>
2020-06-23 17:25:00 +0000

    Add categoryTranslator function

--- core/utils.js
+++ core/utils.js
@@ -28,6 +28,37 @@ async function replaceChar(file, string, replacement){
     });
 }
 
+/**
+ * Translates a category from the database to the correct category 
+ * on Systembolaget's website. Used to build correct links.
+ * @param {String} category 
+ */
+function categoryTranslator(category){
+    sprit = ["Vodka och Br&#228;nnvin", "Akvavit och Kryddat br&#228;nnvin", "Tequila och Mezcal", "Smaksatt sprit", "Gin och Genever", "Calvados",
+     "Anissprit", "Cognac", "Rom", "Drinkar och cocktails", "Grappa och marc", "Frukt och druvsprit", "Punsch", "Lik&#246;r", "Whisky", "Armagnac och brandy",
+    "Bitter", "Sprit av flera typer"]
+    apertif = ["Sake", "Apertif och dessert", "Gl&#246;gg och Gl&#252;hwein", "Bl&#229; stilla", "Bl&#229; mousserande", "Vermouth", "Blandl&#229;dor vin"];
+    roseviner = ["Ros&#233;vin", "Ros&#233; - l&#228;gre alkoholhalt"];
+    presentartiklar = ["Presentf&#246;rpackningar", "Dryckestillbeh&#246;r"];
+    vita_viner = ["Vitt vin", "Vita - l&#228;gre alkoholhalt"];
+    //ol = ["&#214;l"];
+    cider_och_blandrycker = ["Cider", "Blanddrycker"];
+    //alkoholfritt = ["Alkoholfritt"];
+    roda_viner = ["R&#246;tt vin", "R&#246;da - l&#228;gre alkoholhalt"];
+    mousserande_viner = ["Mousserande vin"];
+
+    if(sprit.includes(category)) return "sprit";
+    if(apertif.includes(category)) return "apertif";
+    if(roseviner.includes(category)) return "roseviner";
+    if(presentartiklar.includes(category)) return "presentartiklar";
+    if(vita_viner.includes(category)) return "vita-viner";
+    if(category == "Öl") return "ol";
+    if(cider_och_blandrycker.includes(category)) return "cider-och-blanddrycker";
+    if(category == "Alkoholfritt") return "alkoholfritt";
+    if(roda_viner.includes(category)) return "roda-viner";
+    if(mousserande_viner.includes(category)) return "mousserande-viner";
+}
+
 module.exports = function () {
     this.xlsToCsv = xlsToCsv;
     this.replaceChar = replaceChar;
