3303746ca8683cca0a80867b3bf220b72db04f64
Adam Halim <adaab301@student.liu.se>
2020-07-29 21:04:55 +0000

    Changed Roomie to APK in log

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const server = app.listen(3000, () => {
     const host  = '0.0.0.0';
     const port = 3000;
-    console.log(`Roomie listening on http://${host}:${port}`);
+    console.log(`APK listening on http://${host}:${port}`);
 });
 
 // Routes
